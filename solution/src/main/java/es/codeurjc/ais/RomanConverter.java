8a95daa53812fa99721962cd5013e6464aff9a10
Maes95 <michel.maes95@gmail.com>
2024-03-11 15:04:18 +0100

    Case XX - Refactor

--- solution/src/main/java/es/codeurjc/ais/RomanConverter.java
+++ solution/src/main/java/es/codeurjc/ais/RomanConverter.java
@@ -5,16 +5,14 @@ public class RomanConverter {
     public String convert(int number){
         String roman = "";
 
-        if(number == 10){
-            return "X";
-        }
-
-        if(number == 20){
-            return "XX";
-        }
-
-        for(int i = 0; i < number; i++){
-            roman += "I";
+        if(number >= 10){
+            for(int i = 0; i < number/10; i++){
+                roman += "X";
+            }
+        }else{
+            for(int i = 0; i < number; i++){
+                roman += "I";
+            }
         }
 
         return roman;
