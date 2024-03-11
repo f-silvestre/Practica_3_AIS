a25d57aec24c681c0e4881ed7595937669e44e22
Maes95 <michel.maes95@gmail.com>
2024-03-11 15:09:21 +0100

    Case XI - Second refactor (with errors)

--- solution/src/main/java/es/codeurjc/ais/RomanConverter.java
+++ solution/src/main/java/es/codeurjc/ais/RomanConverter.java
@@ -3,20 +3,21 @@ package es.codeurjc.ais;
 public class RomanConverter {
     
     public String convert(int number){
+        
+        String[] letters = {"I", "X"};
+        int[] values = {1, 10};
+
         String roman = "";
 
         int rest = number;
 
-        while(rest >= 10){
-            roman += "X";
-            rest -= 10;
+        for(int i = 0; i < values.length; i++){
+            while(rest >= values[i]){
+                roman += letters[i];
+                rest -= values[i];
+            }
         }
-
-        while(rest >= 1){
-            roman += "I";
-            rest -= 1;
-        }
-
+        
         return roman;
     }
 }
