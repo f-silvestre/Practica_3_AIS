5e3b85eb12fe716677d7cd969da959336dbbcf78
Maes95 <michel.maes95@gmail.com>
2024-03-11 14:37:45 +0100

    Case III - Refactor Tests

--- solution/src/test/java/es/codeurjc/ais/RomanConverterTest.java
+++ solution/src/test/java/es/codeurjc/ais/RomanConverterTest.java
@@ -2,45 +2,35 @@ package es.codeurjc.ais;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RomanConverterTest {
 
-    // I
-    @Test
-    @DisplayName("Convert 1 to I")
-    public void testI() {
+    @ParameterizedTest(name = "{0} should be {1}")
+    @CsvSource({
+        "I, 1",
+        "II, 2",
+        "III, 3",
+        // "X, 10",
+        // "XX, 20",
+        // "XXX, 30",
+        // "XI, 11",
+        // "IX, 9",
+        // "V, 5",
+        // "IV, 4",
+        // "VI, 6",
+        // "VII, 7",
+        // "VIII, 8",
+        // "L, 50",
+        // "XL, 40",
+        // ...
+    })
+    public void testRoman(String roman, int number) {
         RomanConverter converter = new RomanConverter();
-        assertEquals("I", converter.convert(1));
+        assertEquals(roman, converter.convert(number));
     }
-    // II
-    @Test
-    @DisplayName("Convert 2 to II")
-    public void testII() {
-        RomanConverter converter = new RomanConverter();
-        assertEquals("II", converter.convert(2));
-    }
-    // III
-    @Test
-    @DisplayName("Convert 3 to III")
-    public void testIII() {
-        RomanConverter converter = new RomanConverter();
-        assertEquals("III", converter.convert(3));
-    }
-    // X
-    // XX
-    // XXX
-    // XI
-    // IX
-    // V
-    // IV
-    // VI
-    // VII
-    // VIII
-    // L
-    // XL
-    // ...
+    
     // Zero is not allowed
     // Negative numbers are not allowed
     // 4000 is not allowed
