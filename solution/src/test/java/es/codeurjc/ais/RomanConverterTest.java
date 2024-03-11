cbbd8c2ec04abfff001a8cf8b92ea042d4e8bf95
Maes95 <michel.maes95@gmail.com>
2024-03-11 14:28:17 +0100

    Create solution with list of examples

---
+++ solution/src/test/java/es/codeurjc/ais/RomanConverterTest.java
@@ -0,0 +1,25 @@
+package es.codeurjc.ais;
+
+public class RomanConverterTest {
+
+    // I
+    // II
+    // III
+    // X
+    // XX
+    // XXX
+    // XI
+    // IX
+    // V
+    // IV
+    // VI
+    // VII
+    // VIII
+    // L
+    // XL
+    // ...
+    // Zero is not allowed
+    // Negative numbers are not allowed
+    // 4000 is not allowed
+
+}
