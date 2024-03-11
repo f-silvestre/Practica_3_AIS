8bdc48f3a9d632185e6b7a2d1ec320a499811253
Maes95 <michel.maes95@gmail.com>
2024-03-11 14:22:18 +0100

    Add base code

---
+++ base/src/main/java/es/codeurjc/ais/RomanConverter.java
@@ -0,0 +1,8 @@
+package es.codeurjc.ais;
+
+public class RomanConverter {
+    
+    public String convert(int number){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
